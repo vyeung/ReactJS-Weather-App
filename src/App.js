71135c5ef5c29993e5fad389a08e296660ac2a69
vyeung <vl.yeung@yahoo.com>
2018-04-30 17:58:39 -0400

    save json data to our state

--- src/App.js
+++ src/App.js
@@ -4,15 +4,56 @@ import Titles from "./components/titles"
 import Form from "./components/form"
 import Weather from "./components/weather"
 
+var API_KEY = "d5304abc4f34916873d7ce17376b4847";
+
 //initialize a component
 class App extends React.Component
 {
+    //create an initial state (object)
+    state = {
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: undefined
+    }
+
+    //arrow function to make api call
+    getWeather = async(event) => {
+        //prevent page from auto refreshing when user hits button
+        event.preventDefault();
+
+        //get user input from the form
+        var city = event.target.elements.city.value;
+        var country = event.target.elements.country.value;
+
+        //make api call to OpenWeatherMap using fetch and template string
+        var apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=imperial`);
+    
+        //convert data we get back to json
+        var data = await apiCall.json();
+
+        console.log(data);
+
+        //update our states using the data in our json string
+        this.setState({
+            temperature: data.main.temp,
+            city: data.name,
+            country: data.sys.country,
+            humidity: data.main.humidity,
+            description: data.weather[0].description,
+            error: ""
+        });
+    }
+
     render() {
-        //return JSX code
+        //return JSX code.
+        //setting up props to connect our getWeather function which connects to the components 
         return (
             <div>
                 <Titles />
-                <Form />
+                <Form getWeatherProp={this.getWeather}/>
                 <Weather />
             </div>
         );
--- src/components/form.js
+++ src/components/form.js
@@ -3,10 +3,13 @@ import React from "react";
 class Form extends React.Component
 {
     render() {
+        //let user enter city and country, and give them a button
         return (
-            <div>
-                Form Component
-            </div>
+            <form onSubmit={this.props.getWeatherProp}>
+                <input type="text" name="city" placeholder="City..."/>
+                <input type="text" name="country" placeholder="Country..."/>
+                <button>Get Weather</button>
+            </form>
         ); 
     }
 }

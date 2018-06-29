fd06b862d59230132061459b6f9d8ba4eb716d5b
vyeung <vl.yeung@yahoo.com>
2018-06-28 23:50:28 -0400

    added a 5-Day Forecast button to Form

--- src/components/form.js
+++ src/components/form.js
@@ -3,11 +3,17 @@ import React from "react";
 const Form = (props) => {
   return (
     //let user enter city and country, and give them a button
-    <form onSubmit={props.getWeatherProp}>
-      <input type="text" name="city" placeholder="City..."/>
-      <input type="text" name="country" placeholder="Country..."/>
-      <button>Get Weather</button>
-    </form>
+    <div className="Form">
+       <form onSubmit={props.getWeatherProp}>
+        <input type="text" name="city" placeholder="City..."/>
+        <input type="text" name="country" placeholder="Country..."/>
+        
+        <div>
+          <button className="button">Get Current Weather</button>
+          <button className="button">Get 5-Day Forecast</button>
+        </div>
+      </form>
+    </div>
   );
 }
 

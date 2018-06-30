a56f8909f935fbc8846bf5ff115aad932ca9cf4b
vyeung <vl.yeung@yahoo.com>
2018-06-30 01:45:13 -0400

    changed Form.js to handle both current and 5-day button modes

--- src/components/form.js
+++ src/components/form.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, {Component} from "react";
 
-const Form = (props) => {
-  return (
-    //let user enter city and country, and give them a button
-    <div className="Form">
-       <form onSubmit={props.getWeatherProp}>
-        <input type="text" name="city" placeholder="City..."/>
-        <input type="text" name="country" placeholder="Country..."/>
-        
-        <div>
-          <button className="button">Get Current Weather</button>
-          <button className="button">Get 5-Day Forecast</button>
-        </div>
-      </form>
-    </div>
-  );
+class Form extends Component {
+  state = {
+    isGettingCurrent: null
+  };
+
+  currentHandler = () => {
+    this.setState({isGettingCurrent: true});
+  }
+
+  fiveDayHandler = () => {
+    this.setState({isGettingCurrent: false});
+  }
+  
+  render() {
+    return (
+      //let user enter city and country, and give them 2 buttons to pick weather mode
+      <div className="Form">
+        <form onSubmit={this.state.isGettingCurrent ? this.props.getCurrentWeather : this.props.get5DayForecast}>
+          <input type="text" name="city" placeholder="City..."/>
+          <input type="text" name="country" placeholder="Country..."/>
+          
+          <div>
+            <button className="button" onClick={this.currentHandler}>Get Current Weather</button>
+            <button className="button" onClick={this.fiveDayHandler}>Get 5-Day Forecast</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Form;

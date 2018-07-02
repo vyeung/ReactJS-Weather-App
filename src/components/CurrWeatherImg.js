de8dc128b228e87a75bc5bfb82b314fe18580f8c
vyeung <vl.yeung@yahoo.com>
2018-07-01 21:51:11 -0400

    added className to current weather images

--- src/components/CurrWeatherImg.js
+++ src/components/CurrWeatherImg.js
@@ -10,19 +10,19 @@ const CurrWeatherImg = (props) => {
   let image = null;
 
   if(props.weatherId >= 200 && props.weatherId <= 232)
-    image = <img src={thunderstorm} alt="unknown" />;
+    image = <img className="WeatherImg" src={thunderstorm} alt="unknown" />;
   if(props.weatherId >= 300 && props.weatherId <= 321)
-    image = <img src={rain} alt="unknown" />;
+    image = <img className="WeatherImg" src={rain} alt="unknown" />;
   else if(props.weatherId >= 500 && props.weatherId <= 531)
-    image = <img src={rain} alt="unknown" />;
+    image = <img className="WeatherImg" src={rain} alt="unknown" />;
   else if(props.weatherId >= 600 && props.weatherId <= 622)
-    image = <img src={snow} alt="unknown" />;
+    image = <img className="WeatherImg" src={snow} alt="unknown" />;
   else if(props.weatherId >= 701 && props.weatherId <= 781)
-    image = <img src={other} alt="unknown" />;
+    image = <img className="WeatherImg" src={other} alt="unknown" />;
   else if(props.weatherId === 800)
-    image = <img src={clearSky} alt="unknown" />;
+    image = <img className="WeatherImg" src={clearSky} alt="unknown" />;
   else if(props.weatherId >= 801 && props.weatherId <= 804)
-    image = <img src={clouds} alt="unknown" />;
+    image = <img className="WeatherImg" src={clouds} alt="unknown" />;
 
   return image;
 }
